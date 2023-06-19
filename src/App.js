c4af36e83af3ec8f450135b401b1cf3dfe29246f
irinamihai150 <irinamihai150@gmail.com>
2023-06-19 22:02:16 +0100

    fix loop error

--- src/App.js
+++ src/App.js
@@ -5,28 +5,26 @@ import UserInput from "./components/UserInput"
 
 function App() {
 	const [userInput, setUserInput] = useState(null)
-	const [results, setResults] = useState()
+	const [results, setResults] = useState(null)
 
 	const calculateHandler = (userInput) => {
-		setUserInput(userInput)
-	}
-
-	if (userInput) {
 		let currentSavings = +userInput["current-savings"]
 		const yearlyContribution = +userInput["yearly-contribution"]
 		const expectedReturn = +userInput["expected-return"] / 100
 		const duration = +userInput["duration"]
 		const yearlyData = []
 
-		for (let i = 0; i < duration; i++) {
-			const yearlyInterest = currentSavings * expectedReturn
-			currentSavings += yearlyInterest + yearlyContribution
-			yearlyData.push({
-				year: i + 1,
-				yearlyInterest: yearlyInterest,
-				savingsEndOfYear: currentSavings,
-				yearlyContribution: yearlyContribution,
-			})
+		if (userInput) {
+			for (let i = 0; i < duration; i++) {
+				const yearlyInterest = currentSavings * expectedReturn
+				currentSavings += yearlyInterest + yearlyContribution
+				yearlyData.push({
+					year: i + 1,
+					yearlyInterest: yearlyInterest,
+					savingsEndOfYear: currentSavings,
+					yearlyContribution: yearlyContribution,
+				})
+			}
 		}
 
 		setResults(yearlyData)
