027ea19fb1f337b97262da0342f2a04ff6afd88f
irinamihai150 <irinamihai150@gmail.com>
2023-06-19 21:40:58 +0100

    fix babel error +state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-
+import { useState } from "react"
 import Header from "./components/Header"
 import Table from "./components/Table"
 import UserInput from "./components/UserInput"
 
 function App() {
+	const [userInput, setUserInput] = useState(null)
+	const [results, setResults] = useState()
+
 	const calculateHandler = (userInput) => {
-		// Should be triggered when form is submitted
-		// You might not directly want to bind it to the submit event on the form though...
+		setUserInput(userInput)
+	}
 
-		const yearlyData = [] // per-year results
-
-		let currentSavings = +userInput["current-savings"] // feel free to change the shape of this input object!
-		const yearlyContribution = +userInput["yearly-contribution"] // as mentioned: feel free to change the shape...
+	if (userInput) {
+		let currentSavings = +userInput["current-savings"]
+		const yearlyContribution = +userInput["yearly-contribution"]
 		const expectedReturn = +userInput["expected-return"] / 100
 		const duration = +userInput["duration"]
+		const yearlyData = []
 
-		// The below code calculates yearly results (total savings, interest etc)
 		for (let i = 0; i < duration; i++) {
 			const yearlyInterest = currentSavings * expectedReturn
 			currentSavings += yearlyInterest + yearlyContribution
 			yearlyData.push({
-				// feel free to change the shape of the data pushed to the array!
 				year: i + 1,
 				yearlyInterest: yearlyInterest,
 				savingsEndOfYear: currentSavings,
@@ -28,16 +29,14 @@ function App() {
 			})
 		}
 
-		// do something with yearlyData ...
+		setResults(yearlyData)
 	}
 
 	return (
 		<div>
 			<Header />
-			<UserInput/>
-			{/* Todo: Show below table conditionally (only once result data is available) */}
-			{/* Show fallback text if no data is available */}
-			<Table />
+			<UserInput onCalculate={calculateHandler} />
+			{results ? <Table data={results} /> : <p>No data available.</p>}
 		</div>
 	)
 }
